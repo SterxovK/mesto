0d7f1e3e19a4e2a8e6610c23b10162fadec2c423
Kostya Sterkhov <sterxovk@mail.ru>
2021-12-26 16:42:51 +0400

    feat: сохранения переменных попап

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+let editButton = document.querySelector('.profile-intro__edit-button');
+let popupOpen = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close');
+const POPUP_CLASS = "popup_opened";
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__field_with_name');
+let jobInput = document.querySelector('.popup__field_with_job');
+let introName = document.querySelector('.profile-intro__title');
+let introJob = document.querySelector('.profile-intro__sabtitle');
+
+function EditProfile() {
+  //console.log('Мы кликнули по edit');
+  popupOpen.classList.remove(POPUP_CLASS);
+}
+
+function PopupClose(evt) {
+    evt.preventDefault();
+    //console.log('Мы кликнули по крестику');
+    popupOpen.classList.add(POPUP_CLASS);
+}
+
+editButton.addEventListener('click', EditProfile);
+popupClose.addEventListener('click', PopupClose);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+    introName.textContent = nameInput.value;
+    introJob.textContent = jobInput.value;
+    PopupClose(evt);
+}
+formElement.addEventListener('submit', formSubmitHandler);
