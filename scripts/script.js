b4bc51a66091a3cd0f1eda9a277020e65baa2117
Kostya Sterkhov <sterxovk@mail.ru>
2022-01-05 00:45:56 +0400

    hotfix: иправления после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,34 @@
 let editButton = document.querySelector('.profile-intro__edit-button');
 let popupOpen = document.querySelector('.popup');
-let popupClose = document.querySelector('.popup__close');
+let popupCloseBtn = document.querySelector('.popup__close');
 const popupClass = "popup_opened";
-let formElement = document.querySelector('.popup__container');
+let popupContainer = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__field_with_name');
 let jobInput = document.querySelector('.popup__field_with_job');
 let introName = document.querySelector('.profile-intro__title');
 let introJob = document.querySelector('.profile-intro__sabtitle');
 
-function EditProfile() {
+function editProfile() {
     //console.log('Мы кликнули по edit');
     popupOpen.classList.add(popupClass);
+    nameInput.value = introName.textContent;
+    jobInput.value = introJob.textContent;
 }
 
-function PopupClose(evt) {
+function popupClose(evt) {
     evt.preventDefault();
     //console.log('Мы кликнули по крестику');
     popupOpen.classList.remove(popupClass);
 }
 
-editButton.addEventListener('click', EditProfile);
-popupClose.addEventListener('click', PopupClose);
+editButton.addEventListener('click', editProfile);
+popupCloseBtn.addEventListener('click', popupClose);
 
 function formSubmitHandler(evt) {
     evt.preventDefault(); 
     
     introName.textContent = nameInput.value;
     introJob.textContent = jobInput.value;
-    PopupClose(evt);
+    popupClose(evt);
 }
-formElement.addEventListener('submit', formSubmitHandler);
+popupContainer.addEventListener('submit', formSubmitHandler);
